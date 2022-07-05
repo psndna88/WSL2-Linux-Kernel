eb2c941258f72295c52a134b878e1a493934eac5
psndna88 <psndna88@gmail.com>
2022-07-05 18:39:43 +0530

    WSL2-psndna8_v13.4 5.10.130

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -4,9 +4,9 @@
 KERNELDIR=`readlink -f .`
 
 export AGNI_VERSION="v13.4"
-export AGNI_KERNEL_LINUX="5.10.124"
+export AGNI_KERNEL_LINUX="5.10.130"
 sed -i 's/AGNi_v13.3/AGNi_v13.4/' $KERNELDIR/Microsoft/config-wsl_psndna88
-sed -i 's/5.10.122/5.10.124/' $KERNELDIR/Microsoft/config-wsl_psndna88
+sed -i 's/5.10.124/5.10.130/' $KERNELDIR/Microsoft/config-wsl_psndna88
 
 echo "	AGNi Version info loaded."
 
