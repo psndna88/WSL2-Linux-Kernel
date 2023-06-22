e47c16235ccaf18bfe648390bda0f8ba9dc40c2b
psndna88 <psndna88@gmail.com>
2023-06-22 18:21:54 +0530

    WSL2-psndna88_v14.2 [6.1.35]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,10 +3,10 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v14.1"
-export AGNI_KERNEL_LINUX="6.1.33"
-sed -i 's/AGNi_v14.0/AGNi_v14.1/' $KERNELDIR/Microsoft/config-wsl_psndna88
-sed -i 's/6.1.29/6.1.33/' $KERNELDIR/Microsoft/config-wsl_psndna88
+export AGNI_VERSION="v14.2"
+export AGNI_KERNEL_LINUX="6.1.35"
+sed -i 's/AGNi_v14.1/AGNi_v14.2/' $KERNELDIR/Microsoft/config-wsl_psndna88
+sed -i 's/6.1.33/6.1.35/' $KERNELDIR/Microsoft/config-wsl_psndna88
 
 echo "	AGNi Version info loaded."
 
