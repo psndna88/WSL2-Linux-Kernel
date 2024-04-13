80a4cdcd17334a8a189589fc8097ff01ffc38509
parvinder <parvinder@ifiber.in>
2024-04-14 00:01:55 +0530

    WSL2-tt-psndna88_v15.2 [6.1.86]

--- AGNi_version.sh
+++ AGNi_version.sh
@@ -3,9 +3,9 @@
 ## AGNi version info
 KERNELDIR=`readlink -f .`
 
-export AGNI_VERSION="v15.1"
+export AGNI_VERSION="v15.2"
 export AGNI_KERNEL_LINUX="6.1.86"
-sed -i 's/AGNi_v15.0/AGNi_v15.1/' $KERNELDIR/Microsoft/config-wsl_psndna88
+sed -i 's/AGNi_v15.1/AGNi_v15.2/' $KERNELDIR/Microsoft/config-wsl_psndna88
 sed -i 's/6.1.83/6.1.86/' $KERNELDIR/Microsoft/config-wsl_psndna88
 
 echo "	AGNi Version info loaded."
